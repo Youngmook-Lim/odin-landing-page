b8f268c9c828e4215cdf52be141768a4dde2e5f5
Youngmook Lim <youngmookk@gmail.com>
2022-05-14 11:11:34 +0900

    Add modal html and css

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 
 console.log("hello world");
-const btn = document.querySelector(".cta-btn");
-btn.addEventListener("click", function () {
-  console.log("Hi, I'm a button");
-});
+const btns = document.querySelectorAll(".cta-btn");
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    console.log("Hi, I'm a button");
+  })
+);
